832992ff55bf5788b4989244d31eead09eacf4a2
danbo06 <tanmasaki@gmail.com>
2021-02-25 21:55:59 +0900

    add cargo

---
+++ hello_world/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/danbo06/Documents/Rust_study-main/hello_world/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
+
+/Users/danbo06/Documents/Rust_study-main/hello_world/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/danbo06/Documents/Rust_study-main/hello_world/hello_cargo/target/debug/hello_cargo: /Users/danbo06/Documents/Rust_study-main/hello_world/hello_cargo/src/main.rs
